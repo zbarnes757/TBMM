2c5ed6df36c481f6b0fa55731afb3b0460c0bbef
Hannah Sison & Sam Sedighian <pair+hsison.sedighian@devbootcamp.com>
2015-03-05 15:05:01 -0800

    update redirect and add route for showing survey

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,11 +17,18 @@ end
 
 get '/surveys/:survey_id/new' do
   # sessions for later
-  erb :create_new_question
+  erb :create_new_question # created not sure if works
+end
+
+#display a survey and its questions
+get '/surveys/:survey_id' do
+
 end
 
 post 'questions' do
   # session for later
   # create survey based on params
-  Question.create(content: params[:content])
+  question = Question.create(content: params[:content])
+  redirect '/surveys/??'
+  # redirecting to the survey page but not sure how to get the corresponding survey based on the question
 end
