27d6d3dadb4af9c820dbe0cf95890fa28ea61d1c
Diana Marquez & Greg <pair+dmarquezdlt.gregsantulli@devbootcamp.com>
2015-03-05 15:00:25 -0800

    sign in feature

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,1 +1,17 @@
-index.rb
+get "/" do
+  erb :index
+end
+
+post "/signup" do
+  @user = User.new(params)
+  if @user.save
+    redirect "/#{@user.user_name}"
+  else
+    @errors = @user.errors
+    redirect "/"
+  end
+end
+
+get "/:user_name" do
+  p "we are at your home page"
+end
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+
+  validates :user_name, presence: true, uniqueness: true
+  validates :password_hash, presence: true
+
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash) if password_hash
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(password)
+    self.password == password
+  end
+
+
+end
