8870bb3186b1fc168b27457bd2a28d7299b2e4e4
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-08 17:13:30 -0700

    add routes for signup, login, and logout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,30 @@
 get '/' do
 	erb :index
 end
+
+post '/user/create' do
+	user = User.new(params)
+	if user.save!
+		session[:user_id] = user.id
+		# redirect "/user/#{user.id}"
+	else
+		session[:errors] = user.errors.full_messages
+		redirect '/'
+	end
+end
+
+post '/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+		# redirect "/user/#{user.id}"
+  else
+    flash[:errors] = "Try again"
+    redirect '/'
+  end
+end
+
+get '/logout' do
+	session[:user_id] = nil
+	redirect '/'
+end
