aaf64cd83de87cb26d1bcc668c3799772eeea68b
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-03-11 13:51:28 -0700

    fix image sizes

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -8,7 +8,7 @@ helpers do
   end
 
   def random_beard
-  	beards = ["beards/beard-slogan1.jpeg", "beards/beard-slogan2.jpeg", "beards/beard-slogan3.jpeg", "beards/beard-slogan4.jpeg", "beards/beard-slogan5.jpeg", "beards/beard-slogan6.jpeg", "beards/beard-slogan7.jpeg", "beards/beard-slogan8.jpeg", "beards/beard-slogan9.jpeg", "beards/beard-slogan10.jpeg", "beards/beard-slogan11.jpeg", "beards/beard-slogan12.jpeg", "beards/beard-slogan13.jpeg", "beards/beard-slogan14.jpeg"]
+  	beards = ["beards/beard-slogan1.jpeg", "beards/beard-slogan2.jpeg", "beards/beard-slogan4.jpeg", "beards/beard-slogan5.jpeg", "beards/beard-slogan6.jpeg", "beards/beard-slogan7.jpeg", "beards/beard-slogan9.jpeg", "beards/beard-slogan10.jpeg", "beards/beard-slogan11.jpeg", "beards/beard-slogan12.jpeg", "beards/beard-slogan13.jpeg", "beards/beard-slogan14.jpeg"]
   	return beards.sample
   end
 end
